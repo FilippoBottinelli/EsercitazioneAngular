c8f8cdc6a79b907c48e5e0be074ea995775c9f35
Filippo <filippo.bot@gmail.com>
2020-03-20 15:47:49 +0100

    Lista giochi
    mostro lista giochi

--- src/app/component/games-list-component/games-list-component.component.ts
+++ src/app/component/games-list-component/games-list-component.component.ts
@@ -7,28 +7,37 @@ import { GameItem } from 'src/app/models/game-item';
   styleUrls: ['./games-list-component.component.scss']
 })
 export class GamesListComponentComponent implements OnInit {
-  //lista di giochi
- lista_giochi : GameItem[] =[
-      {
-        id : 1,
-        nome:'Mortal Kombat 11',
-        descrizione : 'Descrizione generica di prova',
-        genere : 'Picchiaduro',
-        rating : 30,
-        prezzo: 70,
-        anno_uscita : new Date(2018, 5, 12)
-      },
-      {
-        id : 2,
-        nome:'DOOM Eternal',
-        descrizione : 'Descrizione generica di prova',
-        genere : 'Sparatutto',
-        rating : 90,
-        prezzo: 50,
-        anno_uscita : new Date(2020, 3, 20)
-      }
-  ]
-  constructor() { }
+  listaGiochi : GameItem [] =[
+    {
+      id : 1,
+      nome:'Mortal Kombat 11',
+      descrizione : 'Descrizione generica di prova',
+      genere : 'Picchiaduro',
+      rating : 30,
+      prezzo: 70,
+      anno_uscita : new Date(1979, 5, 12)
+    },
+    {
+      id : 2,
+      nome:'DOOM Eternal',
+      descrizione : 'Descrizione generica di prova',
+      genere : 'Sparatutto',
+      rating : 90,
+      prezzo: 50,
+      anno_uscita : new Date(2020, 3, 20)
+    },
+    {
+      id : 3,
+      nome:'Injustice 2',
+      descrizione : 'Descrizione generica di prova',
+      genere : 'Picchiaduro',
+      rating : 80,
+      prezzo: 60,
+      anno_uscita : new Date(2020, 3, 20)
+    },
+  ];
+  constructor() { 
+  }
 
   ngOnInit(): void {
   }
