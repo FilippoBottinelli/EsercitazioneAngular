13ad112b950155b617bd899a084c61caca519a59
Filippo <filippo.bot@gmail.com>
2020-03-20 09:17:38 +0100

    Fix
    Correzione cartelle e modifica colori riuscita

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './component/app-component/app-component.component.html',
-  //styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'EsercitazioneAngular';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AppComponentComponent } from './component/app-component/app-component.component';
 import { MenuComponentComponent } from './component/menu-component/menu-component.component';
 import { HomeComponentComponent } from './component/home-component/home-component.component';
 import { GamesListComponentComponent } from './component/games-list-component/games-list-component.component';
@@ -12,7 +11,6 @@ import { EditGameComponentComponent } from './component/edit-game-component/edit
 @NgModule({
   declarations: [
     AppComponent,
-    AppComponentComponent,
     MenuComponentComponent,
     HomeComponentComponent,
     GamesListComponentComponent,
